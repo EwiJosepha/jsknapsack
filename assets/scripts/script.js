bc872a5dd9c870c6cc6632a03bb6960a6ace19e1
ewi josepha <ewijosepha@gmail.com>
2023-09-19 11:05:33 +0200

    building a knapsack

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,20 +1,153 @@
-const rice= document.getElementById('rice')
-const salt= document.getElementById('salt')
-const milk= document.getElementById('milk')
-const oil= document.getElementById('oil')
-const garri= document.getElementById('garri')
-const beans= document.getElementById('beans')
-const tomatoes= document.getElementById('tomatoes')
-const grandnut= document.getElementById('grandnut')
-const cheese= document.getElementById('cheese')
-const butter= document.getElementById('butter')
-const parfumRice= document.getElementById('parfum-rice')
-const mayor= document.getElementById('mayor-oil')
-const cotton= document.getElementById('coton')
-const fish= document.getElementById('fish')
-const meat= document.getElementById('meat')
-const chocolate= document.getElementById('chocolate')
-const egusi= document.getElementById('egusi')
-const maise= document.getElementById('maise')
-const sugar= document.getElementById('sugar')
-const spagetti= document.getElementById('spagetti')
+let rice= document.getElementById('rice')
+let salt= document.getElementById('salt')
+let milk= document.getElementById('milk')
+let oil= document.getElementById('oil')
+let garri= document.getElementById('garri')
+let beans= document.getElementById('beans')
+let tomatoes= document.getElementById('tomatoes')
+let grandnut= document.getElementById('grandnut')
+let cheese= document.getElementById('cheese')
+let butter= document.getElementById('butter')
+let parfumRice= document.getElementById('parfum-rice')
+let mayor= document.getElementById('oilmayor')
+let cotton= document.getElementById('cotton')
+let fish= document.getElementById('fish')
+let meat= document.getElementById('meat')
+let onoin = document.getElementById('onoin')
+let egusi= document.getElementById('egusi')
+let maise= document.getElementById('maise')
+let sugar= document.getElementById('sugar')
+let spaghetti= document.getElementById('spaghetti')
+let btn= document.getElementById('btn')
+
+
+function input () {
+  const inputfield= document.getElementById("input-field").value
+
+  console.log(inputfield)
+}
+
+btn.addEventListener('click', input)
+
+let items = []
+
+function items () {
+  rice + tomatoes + garri + milk
+}
+
+
+
+// egusi = 5
+// cheese = 8
+// butter = 4
+// parfumRice = 5
+// spaghetti = 4
+// rice = 25
+// salt = 10
+// mayor = 8
+// oil = 5
+// milk = 6 
+// maise = 4
+// grandnut = 9
+// meat = 8
+// fish = 13
+// sugar = 3
+// onoin = 4
+// cotton = 50
+// garri = 7
+
+butter.addEventListener("click", ()=>{
+  butter = 4
+  console.log(butter)
+})
+
+egusi.addEventListener("click", ()=>{
+  egusi = 5
+  console.log(egusi)
+})
+
+cheese.addEventListener("click", ()=>{
+  cheese = 8
+  console.log(cheese)
+})
+
+tomatoes.addEventListener("click", ()=>{
+  tomatoes = 7  
+  console.log(tomatoes)
+})
+
+rice.addEventListener("click", ()=>{
+  rice = 25  
+  console.log(rice)
+})
+
+beans.addEventListener("click", ()=>{
+  beans = 5  
+  console.log(beans)
+})
+
+garri.addEventListener("click", ()=>{
+  garri = 7  
+  console.log(garri)
+})
+
+oil.addEventListener("click", ()=>{
+  oil = 5  
+  console.log(oil)
+})
+
+mayor.addEventListener("click", ()=>{
+  mayor = 8  
+  console.log(mayor)
+})
+
+grandnut.addEventListener("click", ()=>{
+  grandnut = 9  
+  console.log(grandnut)
+})
+
+cotton.addEventListener("click", ()=>{
+  cotton = 50  
+  console.log(cotton)
+})
+meat.addEventListener("click", ()=>{
+  meat = 8  
+  console.log(meat)
+})
+fish.addEventListener("click", ()=>{
+  fish = 13  
+  console.log(fish)
+})
+sugar.addEventListener("click", ()=>{
+  sugar = 3  
+  console.log(sugar)
+})
+onoin.addEventListener("click", ()=>{
+  onoin = 4  
+  console.log(onoin)
+})
+
+milk.addEventListener("click", ()=>{
+  milk = 4  
+  console.log(milk)
+})
+
+spaghetti.addEventListener("click", ()=>{
+  spaghetti = 4  
+  console.log(spaghetti)
+})
+
+parfumRice.addEventListener("click", ()=>{
+  parfumRice = 5  
+  console.log(parfumRice)
+})
+
+maise.addEventListener("click", ()=>{
+  maise = 6  
+  console.log(maise)
+})
+
+salt.addEventListener("click", ()=>{
+  salt = 10  
+  console.log(salt)
+})
