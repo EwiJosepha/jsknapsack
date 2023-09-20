86178f4fefc370001ed46fc506318fdf34869e36
ewi josepha <ewijosepha@gmail.com>
2023-09-20 17:28:39 +0200

    building a knapsack

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -136,21 +136,57 @@ let provision = [
 ];
 
 const done = document.getElementById("done");
+const flex = document.querySelector(".flex");
+const finish = document.getElementById("btncomplete");
+const errordisplay = document.getElementById("errodisplay");
+const div = document.createElement('div')
+const p = document.createElement('p')
 
-function input() {
+flex.append(div)
+flex.append(p)
+
+
+finish.addEventListener('click', ()=>{
+ inputt()
+})
+
+
+const displayWeight = document.getElementById('weightdisplay')
+
+function inputt() {
   const input = document.getElementById("input-field").value;
+
+  displayWeight.innerHTML = input
   if (input === "") {
     selectIterms = [];
     console.log("enter a max weight");
     display.innerHTML = "enter max weight first";
-  } 
+  }else if(input<addedWieght){
+    console.log('Error Knapsack cannot contain items please enter weight greater or equal to your maximum weight')
+    div.innerHTML = `<div><p>Error Knapsack cannot contain items please enter weight greater or equal to your maximum weight</p><div>`
+    div.style.backgroundColor = "red"
+    div.style.width= '100%'
+    div.style.padding= '20px'
+    div.style.fontSize= '25px'
+    div.style.fontFamily= 'sans-serif'
+    div.style.height= '120px'
+    div.style.border= '2px solid #fff'
+    div.style.backgroundColor= '#ff0000'
+
+
+  }else{
+    p.innerHTML= 'THIS IS YOUR CURRENT CAPACITY' + ' ' + addedWieght + 'kg'
+    p.style.color = 'green'
+    p.style.fontSize = '25px'
+    p.style.fontFamily = '25px'
+  }
 }
 
 // btn.addEventListener('click', input)
 
 
 done.addEventListener('click', ()=>{
-  input()
+  inputt()
 })
 
 const knapsackIterms = [];
@@ -200,6 +236,8 @@ console.log("added items", knapsackIterms)
 console.log(" selectect weight", event.target.value);
 
 })
+
+
 
 
 
